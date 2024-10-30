9df5249de5a590d70be8f682af73dcef70a69a03
Ovett Mora <ovettmora17@gmail.com>
2024-10-30 00:31:29 -0600

    feat: add DashboardAside component for sidebar navigation

---
+++ src/components/dashboard/DashboardAside.tsx
@@ -0,0 +1,11 @@
+const DashboardAside = () => {
+  return (
+    <div className="fixed top-0 left-0 w-64 h-full bg-deep-navy text-white transition-transform">
+      <section>
+        
+      </section>
+    </div>
+  );
+}
+
+export default DashboardAside;
