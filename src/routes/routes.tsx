6b2dc229aedcb2e0aeab2575171d29fe42f74a44
Ovett Mora <ovettmora17@gmail.com>
2024-10-30 01:30:41 -0600

    feat: add route for Calendar page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../components/login/Login';
 import Dashboard from '../pages/Dashboard';
+import Calendar from '../pages/Calendar';
 const ROUTES = {
   // General Routes
   DASHBOARD: '/',
@@ -13,6 +14,8 @@ const ROUTES = {
 
   // Report Routes
   REPORTS: '/reports',
+
+  CALENDAR: '/calendar',
 };
 
 const AppRoutes = () => {
@@ -21,6 +24,7 @@ const AppRoutes = () => {
       {/* General Routes */}
       <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
+      <Route path={ROUTES.CALENDAR} element={<Calendar />} />
     </Routes>
   )
 };
