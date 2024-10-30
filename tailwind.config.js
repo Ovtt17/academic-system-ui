390e0ac1134868db5b67ebe8b36ae304bd6fc2b0
Ovett Mora <ovettmora17@gmail.com>
2024-10-29 22:05:36 -0600

    feat: update Tailwind CSS configuration with new colors and background settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight-black': '#0b0e14'
+      },
+      backgroundColor: {
+        body: '#F0F4F9',
+        'midnight-black': '#0b0e14',
+        'create-button': '#61DAFA'
+      },
+      boxShadowColor: {
+        'create-button': '#61DAFA80'
+      }
+    },
   },
   plugins: [],
 }
