1de4d3c252735774bdbcc7d99657c02ebb792fa6
Ovett Mora <ovettmora17@gmail.com>
2024-10-30 00:49:25 -0600

    feat: update DashboardAside component layout and integrate UserCard

--- src/components/dashboard/DashboardAside.tsx
+++ src/components/dashboard/DashboardAside.tsx
@@ -1,8 +1,11 @@
+import UserCard from "./UserCard";
+
 const DashboardAside = () => {
   return (
-    <div className="fixed top-0 left-0 w-64 h-full bg-deep-navy text-white transition-transform">
-      <section>
-        
+    <div className="fixed flex-grow top-0 left-0 w-72 h-full bg-deep-navy text-white transition-transform">
+      <section className="flex flex-col p-4 gap-4">
+        <h2 className="text-lg font-semibold">Dashboard</h2>
+        <UserCard />
       </section>
     </div>
   );
