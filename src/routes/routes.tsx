d748841478a8a1d7d4436224875e4504bbac9526
Ovett Mora <ovettmora17@gmail.com>
2024-10-29 22:44:16 -0600

    feat: remove Home component as part of site restructuring

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../components/login/Login';
-import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
 const ROUTES = {
   // General Routes
-  HOME: '/',
+  DASHBOARD: '/',
   LOGIN: '/login',
   REGISTER: '/register',
   ACTIVATE_ACCOUNT: '/activate-account',
@@ -19,7 +19,7 @@ const AppRoutes = () => {
   return (
     <Routes>
       {/* General Routes */}
-      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
     </Routes>
   )
