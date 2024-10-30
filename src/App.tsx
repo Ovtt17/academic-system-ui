49678f173d2988d5cda917456d6a1694c0cfa2ff
Ovett Mora <ovettmora17@gmail.com>
2024-10-29 21:25:22 -0600

    refactor: integrate routing into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
-
+import AppRoutes from './routes/routes';
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="h-screen min-h-screen flex flex-col">
+      <main className="relative p-2 flex-grow">
+        <AppRoutes />
+      </main>
+    </div>
   )
 }
 
