64828c35f320a90da8cde76780200e50a5fb0b3a
Ovett Mora <ovettmora17@gmail.com>
2024-10-30 01:11:50 -0600

    feat: enhance DashboardAside visibility and layout transitions in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,35 @@ import AppRoutes from './routes/routes';
 import Navigation from './components/navigation/Navigation';
 import { useState } from 'react';
 import DashboardAside from './components/dashboard/DashboardAside';
+
 function App() {
-  const [isDashboardAsideVisible, setDashboardAsideVisible] = useState(false);
+  const [isDashboardAsideVisible, setDashboardAsideVisible] = useState(true);
 
   const toggleDashboardAside = () => {
     setDashboardAsideVisible(!isDashboardAsideVisible);
   };
 
   return (
-    <div className="h-screen min-h-screen flex">
-      {isDashboardAsideVisible && (
-        <aside className="transition-transform w-72 bg-deep-navy text-white">
-          <DashboardAside />
-        </aside>
-      )}
-      <div className="flex flex-col flex-grow">
+    <div className="h-screen min-h-screen flex overflow-hidden">
+      <aside
+        className={`transition-transform transform ${isDashboardAsideVisible ? 'translate-x-0' : '-translate-x-full'
+          } w-72 bg-deep-navy text-white fixed inset-y-0 left-0 z-30`}
+      >
+        <DashboardAside />
+      </aside>
+      <div
+        className={`flex flex-col flex-grow transition-transform duration-300 ${isDashboardAsideVisible ? 'ml-72' : 'ml-0'
+          }`}
+      >
         <header>
           <Navigation toggleDashboardAside={toggleDashboardAside} />
         </header>
-
         <main className="relative flex-grow">
           <AppRoutes />
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
