0e9445f49f12ddf60b7af781a4343170ba7535fc
Ovett Mora <ovettmora17@gmail.com>
2024-10-30 20:15:42 -0600

    feat: remove scrollbar styles from DashboardAside for improved UI consistency

--- src/components/dashboard/DashboardAside.tsx
+++ src/components/dashboard/DashboardAside.tsx
@@ -5,7 +5,7 @@ import UserCard from "./UserCard";
 
 const DashboardAside = () => {
   return (
-    <div className="fixed flex-grow top-0 left-0 w-72 h-full bg-deep-navy text-white transition-transform overflow-y-auto scrollbar-thin scrollbar-thumb-gray-950">
+    <div className="fixed flex-grow top-0 left-0 w-72 h-full bg-deep-navy text-white transition-transform overflow-y-auto">
       <section className="flex flex-col p-4 gap-2">
         <h2 className="text-lg font-semibold">Dashboard</h2>
         <UserCard />
