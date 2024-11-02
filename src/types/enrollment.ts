35bd1cfc7f34e9c2ec09f6517e6e288c896bffdf
Ovett Mora <ovettmora17@gmail.com>
2024-11-01 22:41:10 -0600

    refactor: update Enrollment interface to use studentFullName and adjust NewEnrollment type

--- src/types/enrollment.ts
+++ src/types/enrollment.ts
@@ -1,8 +1,10 @@
 export default interface Enrollment {
   id: string;
   courseId: string;
-  studentId: string;
+  studentFullName: string;
   grade: number;
 }
 
-export type NewEnrollment = Omit<Enrollment, 'id'>;
+export type NewEnrollment = Omit<Enrollment, 'id'> & {
+  studentId: string;
+};
