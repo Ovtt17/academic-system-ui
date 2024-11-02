9161b26186defa3bf326c0d13331992d927b5c10
Ovett Mora <ovettmora17@gmail.com>
2024-11-01 22:49:34 -0600

    refactor: update Grade interface to use objects for assignment, course, teacher, and student

--- src/types/grade.ts
+++ src/types/grade.ts
@@ -1,11 +1,21 @@
+import Assignment from "./assignment";
+import Course from "./course";
+import Student from "./student";
+import Teacher from "./teacher";
+
 export default interface Grade {
   id: string;
   grade: number;
   comments?: string;
-  assignmentId: number;
-  courseId: number;
-  teacherId: number;
-  studentId: number;
+  assignment: Assignment;
+  course: Course;
+  teacher: Teacher;
+  student: Student;
 }
 
-export type NewGrade = Omit<Grade, 'id'>;
+export type NewGrade = Omit<Grade, 'id' | 'assigment' | 'course' | 'teacher' | 'student'> & {
+  assignmentId: string;
+  courseId: string;
+  teacherId: string;
+  studentId: string;
+};
