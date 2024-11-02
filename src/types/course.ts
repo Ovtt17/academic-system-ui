1a07af7ad2dacd4ea9e01d701dd70f4deea2965b
Ovett Mora <ovettmora17@gmail.com>
2024-11-01 21:37:49 -0600

    feat: rename Class to Course and update related interfaces for consistency

--- src/types/course.ts
+++ src/types/course.ts
@@ -1,24 +1,24 @@
 import { Assignment } from "./assignment";
-import { ClassSchedule } from "./ClassSchedule";
+import { CourseSchedule } from "./courseSchedule";
 import Student from "./student";
 import { Teacher } from "./teacher";
 
-interface BaseClass {
+interface BaseCourse {
   name: string;
   description: string;
   section: string;
   grade: string;
-  schedules: ClassSchedule[];
+  schedules: CourseSchedule[];
   assignments?: Assignment[];
 }
 
-export interface Class extends BaseClass {
+export interface Course extends BaseCourse {
   id: number;
   students: Student[];
   teacher: Teacher;
 }
 
-export interface NewClass extends BaseClass {
+export interface NewCourse extends BaseCourse {
   teacherId: number;
   studentIds: number[];
 }
