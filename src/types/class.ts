2cd0d73247426e91fc47b5fa931dcf2313aec5a7
Ovett Mora <ovettmora17@gmail.com>
2024-11-01 21:03:15 -0600

    feat: extend Class and NewClass interfaces with additional properties and relationships

--- src/types/class.ts
+++ src/types/class.ts
@@ -1,8 +1,24 @@
 import { Assignment } from "./assignment";
+import { ClassSchedule } from "./ClassSchedule";
+import Student from "./student";
+import { Teacher } from "./teacher";
 
-export interface Class {
-  id: number;
+interface BaseClass {
   name: string;
   description: string;
-  assignments: Assignment[];
+  section: string;
+  grade: string;
+  schedules: ClassSchedule[];
+  assignments?: Assignment[];
+}
+
+export interface Class extends BaseClass {
+  id: number;
+  students: Student[];
+  teacher: Teacher;
+}
+
+export interface NewClass extends BaseClass {
+  teacherId: number;
+  studentIds: number[];
 }
