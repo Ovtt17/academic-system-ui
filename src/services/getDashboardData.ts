a0d396fbdedf340fc765e28b0ee3ec8a8f098071
Ovett Mora <ovettmora17@gmail.com>
2024-11-04 09:09:35 -0600

    feat: enhance getDashboardData function with TypeScript typing and improved fetch options

--- src/services/getDashboardData.ts
+++ src/services/getDashboardData.ts
@@ -1,21 +1,24 @@
+import { DashboardData } from "../types/dashboardData";
+
 const BASE_URL = `${import.meta.env.VITE_API_URL}/dashboard`;
 
 const getHeaders = () => ({
   'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'include': 'credentials',
+  'Accept': 'application/json'
 });
 
-export const getDashboardData = async () => {
+export const getDashboardData = async (): Promise<DashboardData> => {
   try {
     const response = await fetch(BASE_URL, {
       method: 'GET',
       headers: getHeaders(),
+      credentials: 'include',
+      redirect: 'follow',
     });
     if (!response.ok) {
       throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
-    const data = await response.json();
+    const data: DashboardData = await response.json();
     return data;
   } catch (error) {
     console.error('Error fetching dashboard data:', error);
