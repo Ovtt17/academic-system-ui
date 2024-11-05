c5de9b70088838fec7180cffb1722964fa236992
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 13:33:55 -0600

    feat: update CourseSchedule to use DayOfWeek enum for day representation

--- src/types/courseSchedule.ts
+++ src/types/courseSchedule.ts
@@ -1,6 +1,15 @@
+export enum DayOfWeek {
+  MONDAY = 1,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY
+}
 export default interface CourseSchedule {
   id: number;
-  day: Date;
+  day: DayOfWeek;
   startTime: Date;
   endTime: Date;
 }
