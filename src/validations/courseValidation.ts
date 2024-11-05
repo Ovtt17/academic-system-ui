116e617829f19600dd08899fbc3cc2f34597bc25
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 16:29:09 -0600

    feat: enhance course validation to support time string format and validate end time

--- src/validations/courseValidation.ts
+++ src/validations/courseValidation.ts
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 import { DayOfWeek } from '../types/courseSchedule';
+import dayjs from 'dayjs';
 
 export const courseValidation = yup.object().shape({
   name: yup.string().required('Name is required').min(3, 'Name must be at least 3 characters long'),
@@ -9,9 +10,15 @@ export const courseValidation = yup.object().shape({
   teacherId: yup.number().required('Teacher ID is required').positive('Teacher ID must be a positive number').integer('Teacher ID must be an integer'),
   schedules: yup.array().of(
     yup.object().shape({
-      day: yup.mixed<DayOfWeek>().oneOf(Object.values(DayOfWeek) as DayOfWeek[]).required('Day is required'),
-      startTime: yup.date().required('Start Time is required'),
-      endTime: yup.date().required('End Time is required').min(yup.ref('startTime'), 'End Time must be after Start Time'),
+      day: yup.mixed<DayOfWeek>()
+        .oneOf(Object.values(DayOfWeek), 'Invalid day selected')
+        .required('Day is required'),
+      startTime: yup.string().required('Start Time is required'),
+      endTime: yup.string().required('End time is required').test('is-valid-time', 'Invalid end time', value => dayjs(`1970-01-01T${value}:00`).isValid())
+        .test('is-after-start-time', 'End Time must be after Start Time', function (value) {
+          const { startTime } = this.parent;
+          return dayjs(`1970-01-01T${value}:00`).isAfter(dayjs(`1970-01-01T${startTime}:00`));
+        })
     })
   ).required('Schedules are required').min(1, 'At least one schedule is required'),
 });
