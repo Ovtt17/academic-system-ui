5dfec6dd46e3203aa6b7e1cef29ade7e9b50cf94
Ovett Mora <ovettmora17@gmail.com>
2024-11-04 20:03:07 -0600

    feat: add ROUTES constant for application routing configuration

---
+++ src/constants/routes.ts
@@ -0,0 +1,17 @@
+const ROUTES = {
+  // General Routes
+  DASHBOARD: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  ACTIVATE_ACCOUNT: '/activate-account',
+
+  // Profile Routes
+  PROFILE: '/:username',
+
+  // Report Routes
+  REPORTS: '/reports',
+
+  CALENDAR: '/calendar',
+};
+
+export default ROUTES;
