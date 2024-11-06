f18d50d32fd3db24f04bea6675c53f04555cd2a3
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 22:17:23 -0600

    fix: adjust positioning of EditButton for improved layout on different screen sizes

--- src/components/course/EditButton.tsx
+++ src/components/course/EditButton.tsx
@@ -6,7 +6,7 @@ interface EditButtonProps {
 
 const EditButton: React.FC<EditButtonProps> = ({ onClick }) => (
   <button
-    className="absolute top-4 right-4 text-gray-300 hover:text-blue-400 bg-transparent border border-gray-600 rounded-full p-2 transition duration-200 hover:bg-gray-800 focus:outline-none shadow-sm"
+    className="absolute top-2 md:top-4 right-2 md:right-4 text-gray-300 hover:text-blue-400 bg-transparent border border-gray-600 rounded-full p-2 transition duration-200 hover:bg-gray-800 focus:outline-none shadow-sm"
     onClick={onClick}
   >
     <PencilIcon className="h-5 w-5" />
