091d5367a8197a81f3332f63450b1daaaf90d9ca
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 21:41:15 -0600

    feat: add EditButton component for course editing functionality

---
+++ src/components/course/EditButton.tsx
@@ -0,0 +1,16 @@
+import { PencilIcon } from '@heroicons/react/24/solid';
+
+interface EditButtonProps {
+  onClick: () => void;
+}
+
+const EditButton: React.FC<EditButtonProps> = ({ onClick }) => (
+  <button
+    className="absolute top-4 right-4 text-gray-300 hover:text-blue-400 bg-transparent border border-gray-600 rounded-full p-2 transition duration-200 hover:bg-gray-800 focus:outline-none shadow-sm"
+    onClick={onClick}
+  >
+    <PencilIcon className="h-5 w-5" />
+  </button>
+);
+
+export default EditButton;
