bb2211cd1f71c3fd6f1da65f70e51fde15203525
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 22:21:05 -0600

    fix: update Course component background color for improved aesthetics

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -17,7 +17,7 @@ const Courses = () => {
   };
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex bg-deep-navy justify-center items-center">
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
         {courses.map(course => (
           <CourseCard key={course.id} course={course} />
