ba209685d7df4f86fb6ec62eb600fd24e8751be2
Ovett Mora <ovettmora17@gmail.com>
2024-11-11 20:13:46 -0600

    feat: update CreateButton to use Link for navigation and remove modal logic from Courses component

--- src/components/buttons/CreateButton.tsx
+++ src/components/buttons/CreateButton.tsx
@@ -1,23 +1,18 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 interface CreateButtonProps {
-  handleCreate?: () => void;
+  redirectPath: string;
 }
 
-const CreateButton: FC<CreateButtonProps> = ({ handleCreate }) => {
-
-  const handleClick = () => {
-    if (handleCreate) {
-      handleCreate();
-    }
-  };
-
+const CreateButton: FC<CreateButtonProps> = ({ redirectPath }) => {
   return (
+    <Link to={redirectPath}>
     <button
-      onClick={handleClick}
       className='create-button bg-create-button shadow-lg shadow-create-button border-none rounded-full cursor-pointer text-4xl font-bold text-[#FAFAFA] h-[64px] w-[64px] z-10'>
       <span>+</span>
     </button>
+    </Link>
   );
 }
 
