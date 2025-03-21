6c0a3bfb93d8b1b1530bbf31af0c0b6f5c671402
Ovett Mora <ovettmora17@gmail.com>
2025-03-20 22:56:53 -0600

    feat: wrap Navigation component in a header element for improved structure

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -9,20 +9,22 @@ interface NavigationProps {
 
 const Navigation: FC<NavigationProps> = ({ toggleDashboardAside }) => {
   return (
-    <div className="w-full px-2 sm:px-6 lg:px-8">
-      <div className="relative flex h-16">
-        <button className="p-2" onClick={toggleDashboardAside}>
-          <Bars3Icon className="h-8 w-8" />
-        </button>
-        <nav className="absolute right-0 h-16 p-4 text-white">
-          <div className="hidden md:flex items-center space-x-4">
-            <div className="space-x-2 text-bg-card">
-              {mainMenuItems.map(item => <NavButton key={item.title} navItem={item} />)}
+    <header>
+      <div className="w-full px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16">
+          <button className="p-2" onClick={toggleDashboardAside}>
+            <Bars3Icon className="h-8 w-8" />
+          </button>
+          <nav className="absolute right-0 h-16 p-4 text-white">
+            <div className="hidden md:flex items-center space-x-4">
+              <div className="space-x-2 text-bg-card">
+                {mainMenuItems.map(item => <NavButton key={item.title} navItem={item} />)}
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
+        </div>
       </div>
-    </div>
+    </header>
   )
 };
 
