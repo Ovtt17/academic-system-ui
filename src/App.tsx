d0b1971f5ee47ac37eaf105531a0dd49f37f7071
Ovett Mora <ovettmora17@gmail.com>
2025-03-20 22:57:47 -0600

    feat: refactor App component to use AuthProvider and DefaultLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
 import AppRoutes from './routes/routes';
-import Navigation from './components/navigation/Navigation';
-import { useState } from 'react';
-import DashboardAside from './components/menu/DashboardAside';
+import { AuthProvider } from './context/AuthContext';
+import DefaultLayout from './layouts/DefaultLayout';
 
 function App() {
-  const [isDashboardAsideVisible, setDashboardAsideVisible] = useState(false);
-
-  const toggleDashboardAside = () => {
-    setDashboardAsideVisible(!isDashboardAsideVisible);
-  };
-
   return (
-    <div className="h-screen min-h-screen flex overflow-hidden">
-      <aside
-        className={`transition-transform transform ${isDashboardAsideVisible ? 'translate-x-0' : '-translate-x-full'
-          } w-72 bg-deep-navy text-white fixed inset-y-0 left-0 z-30`}
-      >
-        <DashboardAside />
-      </aside>
-      <div
-        className={`flex flex-col flex-grow transition-transform duration-300 ${isDashboardAsideVisible ? 'ml-72' : 'ml-0'
-          }`}
-      >
-        <header>
-          <Navigation toggleDashboardAside={toggleDashboardAside} />
-        </header>
-        <main className="relative flex-grow overflow-y-auto">
-          <AppRoutes />
-        </main>
-      </div>
-    </div>
+    <AuthProvider>
+      <DefaultLayout>
+        <AppRoutes />
+      </DefaultLayout>
+    </AuthProvider>
   );
 }
 
