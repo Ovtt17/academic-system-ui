e9815222dcd42d6c12794b202765c4f675b185b2
Ovett Mora <ovettmora17@gmail.com>
2025-03-22 02:43:20 -0600

    refactor: update assignment service to use AssignmentResponse and handle errors consistently

--- src/services/assignmentService.ts
+++ src/services/assignmentService.ts
@@ -1,32 +1,39 @@
-import Assignment from "../types/assignment";
+import { handleError } from "../components/error/handleError";
+import AssignmentResponse, { AssignmentRequest } from "../types/assignment";
 import axiosInstance from "../utils/axiosInstance";
 
-export const getAssignments = async (): Promise<Assignment[]> => {
+export const getAssignments = async (): Promise<AssignmentResponse[]> => {
   try {
     const response = await axiosInstance.get('/assignments');
     return response.data;
   } catch (error) {
-    console.error('Error fetching assignments:', error);
-    throw error;
+    throw handleError(error);
   }
 }
 
-export const getAssignmentById = async (id: string): Promise<Assignment> => {
+export const getAssignmentById = async (id: number): Promise<AssignmentResponse> => {
   try {
     const response = await axiosInstance.get(`/assignments/${id}`);
     return response.data;
   } catch (error) {
-    console.error('Error fetching assignment:', error);
-    throw error;
+    throw handleError(error);
   }
 }
 
-export const createAssignment = async (assignment: Assignment): Promise<Assignment> => {
+export const createAssignment = async (assignment: AssignmentRequest): Promise<AssignmentResponse> => {
   try {
     const response = await axiosInstance.post('/assignments', assignment);
     return response.data;
   } catch (error) {
-    console.error('Error creating assignment:', error);
-    throw error;
+    throw handleError(error);
+  }
+}
+
+export const updateAssignment = async (id: number, assignment: AssignmentRequest): Promise<AssignmentResponse> => {
+  try {
+    const response = await axiosInstance.put(`/assignments/${id}`, assignment);
+    return response.data;
+  } catch (error) {
+    throw handleError(error);
   }
 }
