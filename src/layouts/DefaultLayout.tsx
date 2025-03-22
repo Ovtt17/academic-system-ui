3e9ffa585cc398337740e77d6de6b5bcc312f081
Ovett Mora <ovettmora17@gmail.com>
2025-03-22 00:08:39 -0600

    feat: update DefaultLayout to manage sidebar visibility based on authentication state

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,20 @@
-import { FC, ReactNode, useState } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import DashboardAside from "../components/menu/DashboardAside";
 import Navigation from "../components/navigation/Navigation";
+import { useAuth } from "../context/AuthContext";
 
 const DefaultLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { isAuthenticated, logout } = useAuth();
 
   const toggleDashboardAside = () => {
     setSidebarOpen(!sidebarOpen);
   };
+
+  useEffect(() => {
+    setSidebarOpen(false);
+  }, [logout]);
+
   return (
     <div className="h-screen min-h-screen flex overflow-hidden bg-deep-navy text-white">
       <DashboardAside isSidebarOpen={sidebarOpen} />
@@ -15,7 +22,7 @@ const DefaultLayout: FC<{ children: ReactNode }> = ({ children }) => {
         className={`flex flex-col flex-grow transition-transform duration-300 ${sidebarOpen ? 'ml-72' : 'ml-0'
           }`}
       >
-        <Navigation toggleDashboardAside={toggleDashboardAside} />
+        {isAuthenticated && <Navigation toggleDashboardAside={toggleDashboardAside} />}
         <main className="relative flex-grow overflow-y-auto p-4">
           {children}
         </main>
