603f37b4c41149b4c91805f02947542f18cb4369
Ovett Mora <ovettmora17@gmail.com>
2025-03-24 12:18:51 -0600

    feat: simplify EditButton component by using FC type for functional component

--- src/components/course/EditButton.tsx
+++ src/components/course/EditButton.tsx
@@ -1,12 +1,13 @@
 import { PencilIcon } from '@heroicons/react/24/solid';
+import {FC} from "react";
 
 interface EditButtonProps {
   onClick: () => void;
 }
 
-const EditButton: React.FC<EditButtonProps> = ({ onClick }) => (
+const EditButton: FC<EditButtonProps> = ({ onClick }) => (
   <button
-    className="absolute top-2 md:top-4 right-2 md:right-4 text-gray-300 hover:text-blue-400 bg-transparent border border-gray-600 rounded-full p-2 transition duration-200 hover:bg-gray-800 focus:outline-none shadow-sm"
+    className="absolute top-2 right-2 text-gray-300 hover:text-blue-400 bg-transparent border border-gray-600 rounded-full p-2 transition duration-200 hover:bg-gray-800 focus:outline-none shadow-sm"
     onClick={onClick}
   >
     <PencilIcon className="h-5 w-5" />
